ddbbab9dfa5397a75a7c238ea0448ec3f6e8a337
Kirill <kirill.kaleychik@mail.ru>
2026-06-23 09:50:06 +0300

    Scroll page

--- nike_com.py
+++ nike_com.py
@@ -17,12 +17,37 @@ def get_info_products(page):
         product = elements.nth(i)
         href = product.locator("a.product-card__link-overlay").get_attribute("href")
         name = product.locator("a.product-card__link-overlay").text_content()
-        price = product.locator("div.product-price").text_content().replace(',','.')
+        price = product.locator("div.product-price").first.text_content().replace(',','.')
         img = product.locator("img.product-card__hero-image").get_attribute("src")
         products.append(
             Sneaker(name, price, href, img, None, [])
         )
     return products
+
+def scroll_to_load_all(page, max_scrolls=50, wait_time=1000):
+    """Scroll down until page height doesn't change"""
+    previous_height = 0
+    scroll_position = 0
+
+    for i in range(max_scrolls):
+        # Get current height
+        current_height = page.evaluate("document.body.scrollHeight")
+
+        print(f"Scroll {i+1}: Height = {current_height}px")
+
+        # If height hasn't changed, we're done
+        if current_height == previous_height and i > 0:
+            print(f"Height stable at {current_height}px. Stopping.")
+            break
+
+        for i in range(50):
+            scroll_position += 200
+            page.evaluate(f"window.scrollTo(0, {scroll_position})")
+            page.wait_for_timeout(wait_time)
+
+        previous_height = current_height
+
+    print(f"Final height: {page.evaluate('document.body.scrollHeight')}px")
 
 
 def nike_search():
@@ -36,11 +61,13 @@ def nike_search():
         page = context.new_page()
 
         # page.goto("https://www.nike.com/lu/en/w/new-mens-basketball-shoes-3glsmz3n82yznik1zy7ok")
-        page.goto("https://www.nike.com/lu/en/w/mens-shoes-nik1zy7ok")
+        # page.goto("https://www.nike.com/lu/en/w/mens-shoes-nik1zy7ok")
+        page.goto("https://www.nike.com/lu/en/w/mens-road-running-shoes-3qxw3znik1zy7ok")
 
         page.wait_for_load_state("load")          # Default - page fully loaded
         close_popup(page)
 
+        scroll_to_load_all(page, 50, 300)
         # get all products from page
         products = get_info_products(page)
 
